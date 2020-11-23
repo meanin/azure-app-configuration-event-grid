7ab4e6c2431f52ce1e57d9442caaf6b13fd9430c
Paweł Ruciński <Pawel.Rucinski@halfords.co.uk>
2020-11-23 10:46:02 +0100

    Add project files.

---
+++ AppConfigurationApplicationPoC.EventGrid/Function1.cs
@@ -0,0 +1,37 @@
+// Default URL for triggering event grid function in the local environment.
+// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using System.Threading.Tasks;
+
+namespace AppConfigurationApplicationPoC.EventGrid
+{
+    public class Function1
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IConfigurationRefresher _configurationRefresher;
+
+        public Function1(
+            IConfiguration configuration, 
+            IConfigurationRefresher configurationRefresher)
+        {
+            _configuration = configuration;
+            _configurationRefresher = configurationRefresher;
+        }
+
+        [FunctionName("Function1")]
+        public async Task Run([EventGridTrigger]EventGridEvent eventGridEvent)
+        {
+            Console.WriteLine($"Before refresh: {_configuration["somekey"]}");
+            var refreshed = await _configurationRefresher.TryRefreshAsync();
+            Console.WriteLine($"Refreshed: {refreshed}");
+            Console.WriteLine($"After refresh: {_configuration["somekey"]}");
+        }
+    }
+}
---
+++ AppConfigurationApplicationPoC.EventGrid/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace AppConfigurationApplicationPoC.EventGrid
+{
+    public class Startup : FunctionsStartup
+    {
+        private static IConfiguration Configuration { set; get; }
+        private static IConfigurationRefresher ConfigurationRefresher { set; get; }
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var services = builder.Services;
+
+            var configurationBuilder = new ConfigurationBuilder()
+                .AddAzureAppConfiguration(options =>
+                {
+                    options.Connect(Environment.GetEnvironmentVariable("AppConfigConnectionString"))
+                        .UseFeatureFlags();
+                    options.Select(KeyFilter.Any, "All");
+
+                    ConfigurationRefresher = options.GetRefresher();
+                });
+
+            Configuration = configurationBuilder.Build();
+
+            services.AddSingleton(Configuration);
+            services.AddSingleton(ConfigurationRefresher);
+        }
+    }
+}
